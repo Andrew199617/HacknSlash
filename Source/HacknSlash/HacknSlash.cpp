7fb3e69e79a18018c89fbd00288757cf3fc6ea17
Andrew199617 <andrewmauro199617@yahoo.com>
2017-05-08 10:37:50 -0600

    Initial Commit.

---
+++ Source/HacknSlash/HacknSlash.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "HacknSlash.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HacknSlash, "HacknSlash" );
+ 
---
+++ Source/HacknSlash/HacknSlashGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameModeBase.h"
+#include "HacknSlashGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AHacknSlashGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AHacknSlashGameMode();
+};
+
+
+
